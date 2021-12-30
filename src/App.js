630814e8ab1f363b065a2c37e5f49a6d5942f7c0
haani0090 <haani0090@gmail.com>
2021-12-30 03:42:42 -0800

    data from api done on console

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@
 import './App.css';
 import Header from './components/Header';
 import CollectionCard from './components/CollectionCard';
+import {useState,useEffect} from "react";
+import axios from "axios";
 
 function App() {
+
+  const [punlistData,setpunklistData] = useState([])
+  useEffect(() =>{
+    const getMyNfts = async ()=>{
+      const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x768e65568d60c8b45BdD47D90BBa1bEd7026E079&order_direction=asc')    
+    console.log(openseaData.data.assets)
+    setpunklistData(openseaData.data.assets)
+    }
+    return getMyNfts()
+  }, [])
+
+
   return (
     <div className='app'>
           <Header />
@@ -11,7 +25,7 @@ function App() {
             id ={0}
             name ={'iz punk'}
             traits ={[{'value': 7}]} 
-            image = ''
+            image = 'https://ipfs.thirdweb.com/ipfs/bafybeiczq5i7rjz5ddp322uz4bfvle5753n56xrnirefosues3lk5getwu'
             />
 
     </div>
